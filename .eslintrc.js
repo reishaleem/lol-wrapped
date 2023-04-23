dbb997edb99e6bfa58bf6a6803f5b8b2039ff837
Dean Haleem <40346044+deanhaleem@users.noreply.github.com>
2023-04-23 12:56:09 -0400

    Feat/Set up monorepo (#1)
    * set up repo
    
    * add github action
    
    * fix pipeline
    
    * rename github action file

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 2,
+    '@typescript-eslint/no-empty-function': 0,
+  },
+};
